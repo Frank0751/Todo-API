39b56c45f9a4dc88fa41379138b920cccf92e1c6
Frank Koomson <frankkoomson1@gmail.com>
2024-10-02 11:56:25 +0000

    Connected to Database - mongoose

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 
 import express from 'express'
+import mongoose from 'mongoose';
+import todoRouter from './routes/todo.js';
+import userRouter from './routes/user.js';
+
+//connect to database
+await mongoose.connect('mongodb+srv://todo-api:todo-api@mest-backend-projects.inq84.mongodb.net//todo-db?retryWrites=true&w=majority&appName=mest-backend-projects')
 //create an express app
-const app = express()
+const app = express();
 
 //Define routes
-app.get('/hello', (req, res, next) => {
-console.log(req.headers);
-res.json('You visited the hello endpoint!');
-});
+// app.get('/hello', (req, res, next) => {
+// console.log(req.headers);
+// res.json('You visited the hello endpoint!');
+// });
 
-app.get('/goodbye', (req, res, next) => {
-    console.log(req.query);
-    res.json('same to you')
-})
+// app.get('/goodbye', (req, res, next) => {
+//     console.log(req.query);
+//     res.json('same to you')
+// })
+
+app.use(todoRouter);
+app.use(userRouter);
 //listen for incoming requests
 app.listen(3000, () => {
     console.log('App is listening on port 3000');
