57386245f2334ba85083ace36a4ee40e634fb111
Frank Koomson <frankkoomson1@gmail.com>
2024-10-02 11:55:35 +0000

    Connected to database - mongoose

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ import express from 'express'
 const app = express()
 
 //Define routes
-app.get('/hello', function(req, res, next) {
+app.get('/hello', (req, res, next) => {
 console.log(req.headers);
 res.json('You visited the hello endpoint!');
 });
 
-app.get('/goodbye', function(req, res, next) {
+app.get('/goodbye', (req, res, next) => {
     console.log(req.query);
     res.json('same to you')
 })
 //listen for incoming requests
-app.listen(3000, function() {
+app.listen(3000, () => {
     console.log('App is listening on port 3000');
 });
